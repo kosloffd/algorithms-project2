88551894da5cf2a5dd6d64dcf27317a6dcb8fecd
FearJoy <john.fitzpatrick.v@gmail.com>
2016-04-26 02:52:49 -0400

    Added and completed functions to count lines and make vector of ints

--- utility.cpp
+++ utility.cpp
@@ -6,7 +6,7 @@ int countLines(std::ifstream& fin, std::string file) {
     fin.seekg(0, std::ios::beg);
 
     // Variable to store the number of lines
-    int count = -1;
+    int lineCount = -1;
 
     // Ensure file is open
     if (!fin.is_open()) {
@@ -15,16 +15,48 @@ int countLines(std::ifstream& fin, std::string file) {
     }
 
     // Count lines using STL
-    // Source: http://stackoverflow.com/questions/3072795/how-to-count-lines-of-a-file-in-c
+    // Source:
+    // http://stackoverflow.com/questions/3072795/how-to-count-lines-of-a-file-in-c
     if (fin.is_open()) {
-        count = std::count(std::istreambuf_iterator<char>(fin), std::istreambuf_iterator<char>(), '\n');
+        lineCount = std::count(std::istreambuf_iterator<char>(fin), std::istreambuf_iterator<char>(), '\n');
     }
 
     // Clear EOF flag and move cursor to the first character in the file
     fin.clear();
     fin.seekg(0, std::ios::beg);
 
-    // Debugging output
-    std::cout << "\nCount of lines is: " << count << "!!!\n";
-    return count;
+    return lineCount;
+}
+
+std::vector<int> getValues(std::ifstream &fin, std::string file) {
+    std::vector<int> arrValues;
+    std::string str;
+    int temp;
+
+    getline(fin, str);
+
+    // Here we read a line from the file in the format
+    // [x, x, x, x, x, x]
+    // Where x is any positive integer. First we remove any spaces
+    // and brackets. Now we have a comma delimited string as
+    // x,x,x,x,x,x
+    str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+    str.erase(std::remove(str.begin(), str.end(), '['), str.end());
+    str.erase(std::remove(str.begin(), str.end(), ']'), str.end());
+    std::cout << str << std::endl;
+
+    // Load the string str into a string stream
+    std::stringstream ss(str);
+
+    int i;
+
+    // String stream will pull each int out, and we make it
+    // ignore any commas it finds
+    while (ss >> i) {
+        arrValues.push_back(i);
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+
+    return arrValues;
 }
--- utility.h
+++ utility.h
@@ -1,11 +1,17 @@
-#ifndef CLION_PROJECT2_UTILITY_H
-#define CLION_PROJECT2_UTILITY_H
+#ifndef UTILITY_H
+#define UTILITY_H
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
 
 
 int countLines(std::ifstream& fin, std::string str);
+
+std::vector<int> getValues(std::ifstream &fin, std::string file);
 
 #endif
